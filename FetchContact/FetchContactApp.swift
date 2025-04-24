6ffdc2d786a81fc5a685328ba05ed65a730fcdbd
Daffnel <3ggapp24.andrda@folkuniversitetet.nu>
2025-04-24 21:00:23 +0200

    Initial Commit

---
+++ FetchContact/FetchContactApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchContactApp.swift
+//  FetchContact
+//
+//  Created by Daniel A on 2025-04-24.
+//
+
+import SwiftUI
+
+@main
+struct FetchContactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchContactTests/FetchContactTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchContactTests.swift
+//  FetchContactTests
+//
+//  Created by Daniel A on 2025-04-24.
+//
+
+import Testing
+@testable import FetchContact
+
+struct FetchContactTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
